51f5fc83bc4dd09eccfd14d4b0250fe3f8cdf6cf
Yasin Arafat <yasinarafat1194@gmail.com>
2021-07-29 01:48:04 +0600

    day 2 nodemon add

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express=require('express');
 const app=express();
 
+app.get('/hi',(req,res)=>{
+    return res.send('yes no');
+
+})
+
 app.listen(2500,function(){
     console.log('server is running at port 2500');
 });
