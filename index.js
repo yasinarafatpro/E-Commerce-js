3bf6862fad1160c2ac949f3ff65edf7b9c3a9bf7
Yasin Arafat <yasinarafat1194@gmail.com>
2021-07-31 18:36:21 +0600

    view engine add

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-  return res.send('Yasin Arafat')
+  return res.render('index')
 })
 
 app.listen(2500, function () {
