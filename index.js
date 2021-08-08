a6e7d1732f36dd27a29ffb4b990bdd4aaadaf727
Yasin Arafat <yasinarafat1194@gmail.com>
2021-08-08 15:12:26 +0600

    session

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
+/* eslint-disable no-lone-blocks */
 const express = require('express')
 const bodyParser = require('body-parser')
 require('./utils/db.config')
 const authRouths = require('./routes/authRoutes')
 const app = express()
-
+const session = require('express-session')
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs')
 
+// app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: '0dceb32d28e90a39d5803eb2a35f5fb25009f38f',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+app.use('/', authRouths)
+
 app.get('/', (req, res) => {
+  req.session.views = (req.session.views || 0) + 1
+  console.log(`you have visited ${req.session.views} times`)
+
   return res.render('index')
 })
-app.use('/', authRouths)
 
 app.listen(2500, function () {
   console.log('server is running at port 2500')
